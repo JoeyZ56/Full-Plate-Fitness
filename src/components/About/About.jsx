b8e43e69220cc85cb354679c95533d142994ff58
username <Jzazzi56@gmail.comn>
2023-10-19 14:52:30 -0700

    found more escaped quotes to fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -30,17 +30,17 @@ const About = () => {
           From Struggle to Strength In 2018, my fitness journey began, fueled by
           dissatisfaction with my health. A decade of on-and-off gym visits had
           left me overweight and unhappy. Enter CrossFit—an initial love affair
-          with heavy weights that, while invigorating, wasn't sustainable for
-          me. Shifting to dedicated strength training five years ago proved
+          with heavy weights that, while invigorating, wasn&apos;t sustainable
+          for me. Shifting to dedicated strength training five years ago proved
           transformative. Beyond physical changes, this journey ignited mental
           clarity and better nutritional choices. Recognizing the impact on
           anxiety and depression, I felt a calling to help others. Through
           education, my passion is guiding you to embrace the process, making
-          fitness a sustainable joy. Whether it's strength, a healthier
-          lifestyle, intuitive eating, or everyday feats, I'm here to support
-          your well-being journey.
+          fitness a sustainable joy. Whether it&apos;s strength, a healthier
+          lifestyle, intuitive eating, or everyday feats, I&apos;m here to
+          support your well-being journey.
           <br />
-          <b>Let's make your goals a reality—together.</b>
+          <b>Let&apos;s make your goals a reality—together.</b>
         </p>
       </div>
     </div>
