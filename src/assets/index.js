e0f5b498b24325362a5a53c1c1c526339ccd3018
username <Jzazzi56@gmail.comn>
2023-11-01 15:15:16 -0700

    who would of thought a quote in a p tag would break my build lol

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,11 @@
-import jensPhoto from "./jens-photo.jpeg";
+import jensPhoto from "./profilepic.png";
 import logo from "./logo.png";
 import food from "./food.png";
 import weights from "./weights.png";
 import email from "./email.png";
 import phone from "./phone.png";
+import slide1 from "./slide1.png";
+import slide2 from "./slide2.png";
+import slide3 from "./slide3.png";
 
-export { jensPhoto, logo, food, weights, email, phone };
+export { jensPhoto, logo, food, weights, email, phone, slide1, slide2, slide3 };
